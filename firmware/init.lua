72e165141a3535450f28ca13ed9232dac81a21d5
ttosi <ttosi519@gmail.com>
2016-09-04 21:21:02 -0700

    client started, not tested

--- firmware/init.lua
+++ firmware/init.lua
@@ -1,20 +1,9 @@
 uart.setup(0, 9600, 8, 0, 1, 0)
-print("initialzed")
 
-if file.open("config.lua", "r") then
-	settings = file.readline()
-	file.close()
+wifi.setmode(wifi.STATION)
+wifi.sta.config("COLUMBIAVIEW", "j3nny$int3rnet")
 
-	ssid, psk, deskname = string.match(settings, "ssid={(.+)}:psk={(.+)}:deskname={(.+)}")
-    print("ssid: ", ssid)
-    print("psk: ", psk)
-
-    wifi.setmode(wifi.STATION)
-	wifi.sta.config(ssid, psk)
-
-	tmr.alarm(0, 3000, 0, function()
-		--dofile("client.lua")
-        print("client started")
-
-	end)
-end
+tmr.alarm(0, 3000, 0, function()
+	print(wifi.sta.getip())
+	dofile("client.lua")
+end)
